0a84b94ee55169613f0703a303f5158a68ba1ba3
ColdCoder92 <lucasrodriguez1002@proton.me>
2025-04-22 23:34:01 -0400

    Setup Property View & Added Basic Links

---
+++ Project-2.Pages/Pages/Shared/_Layout.cshtml.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Project_2.Models;
+
+namespace Project_2.Pages.Pages {
+    public class LayoutModel(
+    ILogger<IndexModel> logger,
+    UserController userController
+    ): PageModel {
+        private readonly ILogger<IndexModel> _logger = logger;
+        private readonly UserController _userController = userController;
+
+        public new User? User {get; set;}
+
+        public bool IsLoggedIn() {
+            return User is not null;
+        }
+
+        public IActionResult OnLogout()
+        {
+            User = null;
+            return RedirectToPage("./Index");
+        }
+    }
+}
