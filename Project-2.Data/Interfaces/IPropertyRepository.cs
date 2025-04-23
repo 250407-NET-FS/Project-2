bdf8461024b5fb3436ef4c5223e28bf943d4dbfd
Andric Tam <andtam1999@gmail.com>
2025-04-23 13:38:30 -0400

    Removed additional classes

--- Project-2.Data/Interfaces/IPropertyRepository.cs
+++ Project-2.Data/Interfaces/IPropertyRepository.cs
@@ -3,6 +3,6 @@ using Project_2.Models;
 namespace Project_2.Data;
 
 public interface IPropertyRepository : IBaseRepository<Property> {
-    public Task<IEnumerable<Property>> GetAllWithFilters(decimal price);
-    public void Update();
+    public Task<IEnumerable<Property>> GetAllWithFilters(decimal priceMin, decimal priceMax, int numBedroom, double num);
+    public void Update(Property propertyInfo);
 }
