63d6182baaefa08c7277cc0027f3afd13eb1215b
ColdCoder92 <lucasrodriguez1002@proton.me>
2025-04-22 23:34:01 -0400

    Setup Property View & Added Basic Links

--- Project-2.Pages/Pages/Index.cshtml.cs
+++ Project-2.Pages/Pages/Index.cshtml.cs
@@ -1,19 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Project_2.Models;
 
 namespace Project_2.Pages.Pages;
 
-public class IndexModel : PageModel
+public class IndexModel(
+    ILogger<IndexModel> logger,
+    UserController userController,
+    PropertyController propertyController
+    ) : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
+    private readonly ILogger<IndexModel> _logger = logger;
+    private readonly UserController _userController = userController;
+    private readonly PropertyController _propertyController = propertyController;
 
-    public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
+    public new User? User {get; set;}
+    public required IList<Property> Properties {get; set;}
+    public int Offset {get; set;} = 0;
+    public int Capacity {get; set;}
+    // May be changed after implementing OAuth
+    public async Task OnGetUserAsync() {
+        User = await _userController.GetAsync();
     }
 
-    public void OnGet()
-    {
+    public async Task OnGetPropertyListAsync() {
+        Properties = await _propertyController.GetAllAsync();
+    }
 
+    public void OnIncrementPropertyOffset() {
+        if (Offset < Properties.Count - 1) {
+            Offset++;
+        }
+    }
+    public void OnDecrementPropertyOffset() {
+        if (Offset > 0) {
+            Offset--;
+        }
     }
 }
