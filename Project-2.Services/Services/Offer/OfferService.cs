a6cde86f69e31d1adb6a59da5fd1183ac9ba1f6d
RLopez149 <roberto.lopez736146@gmail.com>
2025-04-24 13:38:00 -0500

    Offer service done

--- Project-2.Services/Services/Offer/OfferService.cs
+++ Project-2.Services/Services/Offer/OfferService.cs
@@ -2,15 +2,17 @@ using Microsoft.EntityFrameworkCore;
 using Project_2.Data;
 using Project_2.Models;
 
-namespace Project_2.Services.Services;
+namespace Project_2.Services;
 
 public class OfferService : IOfferService
 {
     private readonly IOfferRepository _offerRepository;
+    private readonly IPropertyRepository _propertyRepository;
 
-    public OfferService(IOfferRepository offerRepository)
+    public OfferService(IOfferRepository offerRepository, IPropertyRepository propertyRepository)
     {
         _offerRepository = offerRepository;
+        _propertyRepository = propertyRepository;
     }
 
     public async Task<IEnumerable<Offer>> GetAllAsync()
@@ -18,30 +20,31 @@ public class OfferService : IOfferService
         return await _offerRepository.GetAllAsync();
     }
 
-    public async Task<Offer?> GetByIdAsync(Guid guid)
+    public async Task<Offer?> GetByIdAsync(Guid id)
     {
-        return await _offerRepository.GetByIdAsync(guid);
+        return await _offerRepository.GetByIdAsync(id);
     }
 
     public async Task AddAsync(Offer offer)
     {
+        // check if offer is good
+        if (offer is null)
+            throw new Exception("offer cannot be null");
+
+        // add to database
         await _offerRepository.AddAsync(offer);
+        // save database
         await _offerRepository.SaveChangesAsync();
     }
 
-    public async Task RemoveAsync(Offer offer)
+    public async Task<IEnumerable<Offer>> GetAllForProperty(Guid propertyId)
     {
-        _offerRepository.Remove(offer);
-        await _offerRepository.SaveChangesAsync();
-    }
+        // check if property exist
+        Property property = await _propertyRepository.GetByIdAsync(propertyId);
+        if (property is null)
+            throw new Exception("Property doees not exist");
 
-    public async Task<IEnumerable<Offer>> GetAllForProperty()
-    {
-        return await _offerRepository.GetAllAsync();
-    }
-
-    public async Task<IEnumerable<Offer>> GetAllByUser()
-    {
-        return await _offerRepository.GetAllAsync();
+        // return list of offers for property
+        return await _offerRepository.GetAllForProperty(propertyId);
     }
 }
