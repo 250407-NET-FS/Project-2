706ed40b50b6fcf55931448e62109b196bdd3b81
RLopez149 <roberto.lopez736146@gmail.com>
2025-04-28 08:14:51 -0500

    added DTOs to service and made test

--- Project-2.Services/Services/Offer/IOfferService.cs
+++ Project-2.Services/Services/Offer/IOfferService.cs
@@ -1,11 +1,15 @@
 using Project_2.Models;
+using Project_2.Models.DTOs;
 
 namespace Project_2.Services;
 
 public interface IOfferService
 {
-    Task<IEnumerable<Offer>> GetAllAsync();
-    Task<Offer?> GetByIdAsync(Guid guid);
-    Task AddAsync(Offer offer);
-    Task<IEnumerable<Offer>> GetAllForProperty(Guid propertyId);
+    public Task<IEnumerable<OfferResponseDTO>> GetAllAsync();
+    public Task<OfferResponseDTO?> GetByIdAsync(Guid id);
+    public Task<OfferResponseDTO> AddAsync(OfferNewDTO dto);
+    public Task RemoveAsync(Guid offerId);
+    public Task<IEnumerable<OfferResponseDTO>> GetAllForProperty(Guid propertyId);
+    public Task<IEnumerable<OfferResponseDTO>> GetAllByUser(Guid userId);
+    public Task<IEnumerable<OfferResponseDTO>> SearchOffersAsync(OfferSearchDTO dto);
 }
