838c6ba4f4c8466116488e0b60116f4f072577fd
ColdCoder92 <lucasrodriguez1002@proton.me>
2025-04-29 16:53:23 -0400

    Scaffold Deactivate & Ban Button Functionality

--- Project-2.API/Pages/Admin/AdminUserList.cshtml.cs
+++ Project-2.API/Pages/Admin/AdminUserList.cshtml.cs
@@ -1,10 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Identity;
 using Project_2.API;
 using Project_2.Models;
 
 namespace Project_2.Pages.Pages.Admin {
-    class UserModel(UserController controller): PageModel {
+    class UserModel(
+        ILogger<LayoutModel> logger, UserManager<User> userManager, UserController controller
+        ): LayoutModel(logger, userManager) {
+        private readonly UserManager<User> _userManager = userManager;
         private readonly UserController _controller = controller;
 
         public async Task<IActionResult> OnGetAsync() {
@@ -14,6 +18,25 @@ namespace Project_2.Pages.Pages.Admin {
 
         [BindProperty]
         public List<User>? UserList {get; set;}
+
+        // public async Task<IActionResult> OnDeactivate(Guid id) {
+        //     if (!UserList.Find(u => u.Id.Equals(id)).isDeactivated) {
+        //         UserList.Find(u => u.Id.Equals(id)).isDeactivated = true;
+        //     }
+        //     else {
+        //         UserList.Find(u => u.Id.Equals(id)).isDeactivated = false;
+        //     }
+        // }
+
+
+        // public async Task<IActionResult> OnBan(Guid id) {
+        //     if (!UserList.Find(u => u.Id.Equals(id)).isBanned) {
+        //         UserList.Find(u => u.Id.Equals(id)).isBanned = true;
+        //     }
+        //     else {
+        //         UserList.Find(u => u.Id.Equals(id)).isBanned = false;
+        //     }
+        // }
 
         public async Task<IActionResult> OnDeleteAsync(Guid id) {
             // User user = _controller.DeleteUserById(id);
