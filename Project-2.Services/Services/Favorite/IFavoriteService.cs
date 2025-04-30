3483c0f51300926b551b51177d374cf05f4cb024
Andric Tam <andtam1999@gmail.com>
2025-04-30 17:21:54 -0400

    even more favorites fixes

--- Project-2.Services/Services/Favorite/IFavoriteService.cs
+++ Project-2.Services/Services/Favorite/IFavoriteService.cs
@@ -7,7 +7,7 @@ public interface IFavoriteService
 {
     Task<IEnumerable<Favorite>> GetAllFavoritesAsync();
     Task MarkUnmarkFavoriteAsync(FavoritesDTO dto);
-    Task<bool> CheckFavoritedAsync(FavoritesDTO dto);
+    Task<bool> CheckFavoritedAsync(FavoritesGetDTO dto);
     Task<IEnumerable<FavoritesDTO>> GetAllForPropertyAsync(Guid propertyId); // unused stretch goal 
     Task<IEnumerable<FavoriteListForUserDTO>> GetAllByUserAsync(Guid userId);
 }
