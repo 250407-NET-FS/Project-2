dc74976131c8f76e9f89993ad1ff073c4478a64a
hillbillyjoe41 <157071173+hillbillyjoe41@users.noreply.github.com>
2025-05-01 11:00:17 -0500

    added property retrievel razor page backend

--- Project-2.API/Pages/EstateProperties/Retrieve.cshtml.cs
+++ Project-2.API/Pages/EstateProperties/Retrieve.cshtml.cs
@@ -4,21 +4,26 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Project_2.API;
 using Project_2.Models;
+using Project_2.Models.DTOs;
 
 namespace Project_2.Pages.Pages.EstateProperties {
     public class RetrieveModel(
             ILogger<LayoutModel> logger,
             UserManager<User> manager,
             PropertyController propertyController,
-            FavoriteController favoriteController
+            FavoriteController favoriteController,
+            UserController userController,
+            Favorite favorite
             ): LayoutModel(logger, manager) {
         private readonly PropertyController _propertyController = propertyController;
         private readonly FavoriteController _favoriteController = favoriteController;
         private readonly ILogger<LayoutModel> _logger = logger;
 
+        private readonly UserController _userController = userController;
+
         public async Task<IActionResult> OnGet(Guid id) {
-            //Property = await _propertyController.GetPropertyById(id);
-            //User = await User.getUserById(Property.OwnerID);
+            Property = await GetProperty(id);
+            User = await manager.FindByIdAsync(Property.OwnerID.ToString());
             DaysListed = DateTime.Now.Subtract(Property!.ListDate).Days;
             return Page();
         }
@@ -26,21 +31,27 @@ namespace Project_2.Pages.Pages.EstateProperties {
         [BindProperty]
         public Property? Property {get; set;}
         public new User? User {get; set;}
-        //public FavoriteDto? BookmarkInfo {get; set;}
+        public FavoritesDTO? BookmarkInfo {get; set;}
         public bool IsSaved {get; set;}
         public int DaysListed {get; set;}
 
-        public async Task<IActionResult> BookmarkAsync() {
+        public async Task<IActionResult> BookmarkAsync(FavoritesDTO dto) {
             if (IsSaved) {
-                //await _favoriteController.RemoveFavorite();
+                await _favoriteController.MarkUnmarkFavorite(dto);
                 IsSaved = false;
             }
             else {
-                //await _favoriteController.CreateFavorite(BookmarkInfo);
+                await _favoriteController.MarkUnmarkFavorite(dto);
                 IsSaved = true;
             }
 
             return RedirectToPage();
+        }
+
+        public async Task<Property> GetProperty(Guid id)
+        {
+            var property = await _propertyController.GetPropertyById(id);
+            return property.Value;
         }
     }
 }
