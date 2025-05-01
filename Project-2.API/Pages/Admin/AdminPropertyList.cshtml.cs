d89fbc717d1757371f3e17750c534295238f3a2b
Amathost <124377028+Amathost@users.noreply.github.com>
2025-05-01 12:42:06 -0400

    Admin property List works

--- Project-2.API/Pages/Admin/AdminPropertyList.cshtml.cs
+++ Project-2.API/Pages/Admin/AdminPropertyList.cshtml.cs
@@ -1,30 +1,39 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Project_2.API;
 using Project_2.Models;
+using Project_2.Services.Services;
 
-namespace Project_2.Pages.Pages.Admin {
-    class PropertyModel(PropertyController propertyController, UserController userController): PageModel {
-        private readonly PropertyController _propertyController = propertyController;
-        private readonly UserController _userController = userController;
+namespace Project_2.Pages.Pages.Admin
+{
+    class PropertyModel(PropertyService propertyService, ILogger<IndexModel> logger,
+       UserManager<User> userManager
+       ) : LayoutModel(logger, userManager)
+    {
+        private readonly PropertyService _propertyService = propertyService;
+        private readonly UserManager<User> _userManager = userManager;
 
-        public async Task<IActionResult> OnGetAsync() {
-            //PropertyList = await _controller.GetAllProperties();
-            // foreach (Property property in PropertyList) {
-            //     User owner = _userController.GetUserById(property.OwnerID);
-            //     OwnerList!.Add(owner);
-            // }
+        public async Task<IActionResult> OnGetAsync()
+        {
+            PropertyList = (List<Property>?)await _propertyService.GetPropertiesAsync("", "", "", "", "", -1, -1, -1, -1, false, null);
+
+            foreach (Property property in PropertyList)
+            {
+                User owner = await _userManager.FindByIdAsync(property.OwnerID.ToString());
+                OwnerList!.Add(owner);
+            }
             return Page();
         }
 
         [BindProperty]
-        public List<Property>? PropertyList {get; set;}
-        public List<User> OwnerList {get; set;} = [];
+        public List<Property>? PropertyList { get; set; }
+        public List<User> OwnerList { get; set; } = [];
 
-        public async Task<IActionResult> OnDeleteAsync(Guid id) {
-            // Property property = _controller.DeletePropertyAdmin(id);
-            // PropertyList!.Remove(property);
-            return Page();
+        public async Task<IActionResult> OnPostDeleteAsync(Guid Propertyid, Guid OwnerId)
+        {
+            await _propertyService.RemovePropertyAsync(Propertyid, OwnerId);
+            return RedirectToPage();
         }
     }
 }
