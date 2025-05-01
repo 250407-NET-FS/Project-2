553e7d6d85aaf51347d1c26952232083c28c3b18
Amathost <124377028+Amathost@users.noreply.github.com>
2025-05-01 12:35:14 -0400

    admin index working

--- Project-2.API/Pages/Admin/AdminIndex.cshtml.cs
+++ Project-2.API/Pages/Admin/AdminIndex.cshtml.cs
@@ -2,18 +2,22 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Project_2.Models;
 using Project_2.API;
+using Microsoft.AspNetCore.Identity;
 
 namespace Project_2.Pages.Pages.Admin;
 
 public class AdminIndexModel(
     ILogger<IndexModel> logger,
-    UserController userController,
+    UserManager<User> userManager,
     PropertyController propertyController
-    ) : PageModel
+
+    ) : LayoutModel(logger, userManager)
 {
     private readonly ILogger<IndexModel> _logger = logger;
-    private readonly UserController _userController = userController;
+    private readonly UserManager<User> _userManager = userManager;
     private readonly PropertyController _propertyController = propertyController;
+
+
 
     public User? Admin {get; set;}
 }
